5b45f8b0ce385212f5aae38f65b745998afec709
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-04-22 11:32:54 -0700

    Updates Client to test provider code

--- src/main/java/pantry/Client.java
+++ src/main/java/pantry/Client.java
@@ -3,5 +3,14 @@ package pantry;
 public class Client {
   public static void main(String[] args) {
     System.out.println("Spring Food Pantry");
+    Provider provider = new Organization("Hunger Fighters");
+    Provider provider2 = new CommunityMember("Annie Bidwell");
+    System.out.println("Provider UUID: " + provider.get_provider_info());
+    provider.add_item("94011", "Bananas", 0.99, true, 14, 10.5);
+    provider.add_item("3424", "Carrots", 0.75, true, 21, 5.75);
+    provider.show_items();
+    System.out.println("Provider UUID: " + provider2.get_provider_info());
+    provider2.add_item("894455000322", "Almond Butter", 0.0, false, 180, 5.0);
+    provider2.show_items();
   }
 }
