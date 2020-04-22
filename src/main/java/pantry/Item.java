df6acdeb179971baf505f5c7dd75cef3af655f9c
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-04-22 11:31:03 -0700

    adds Item class

---
+++ src/main/java/pantry/Item.java
@@ -0,0 +1,12 @@
+package pantry;
+
+import java.time.LocalDate;
+
+public class Item{
+  public String code;
+  public String name;
+  public double cost;
+  public boolean plu;
+  public LocalDate date_received;
+  public LocalDate exp_date;
+}
