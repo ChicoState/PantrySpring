56cbff26e0bada2dd0813eb6fb2c14dc27ae6dba
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-04-27 21:54:51 -0700

    updates Provider to be a class (not interface)

--- src/main/java/pantry/Provider.java
+++ src/main/java/pantry/Provider.java
@@ -1,10 +1,85 @@
-package pantry;
+package main.java.pantry;
 
+import java.util.UUID;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.text.NumberFormat;
+import java.util.HashMap;
 import java.util.Vector;
 
-public interface Provider {
-  public Vector<String> get_provider_info();
+public class Provider {
+  private final UUID _uuid;
+  private final String _name;
+  private final ProviderType _type;
+
+  // List of all items being donated/sold
+  public Vector<Item> item_list = new Vector<>();
+  // String code for each item + quantity of each item
+  public HashMap<String, Double> donated_sold = new HashMap<>();
+
+  // Constructor
+  // create a provider with random UUID, given name, and type (organization or community member)
+  Provider(String name, String type) {
+    _uuid = UUID.randomUUID();
+    _name = name;
+    if(type == "organization"){
+      _type = ProviderType.ORGANIZATION;
+    }
+    else{
+      _type = ProviderType.COMMUNITYMEMBER;
+    }
+  }
+
+  // Return the UUID, name, and type of this Provider
+  public Vector<String> get_provider_info(){
+    Vector<String> provider = new Vector<>();
+    provider.add(_uuid.toString());
+    provider.add(_name);
+    provider.add(String.valueOf(_type));
+    return provider;
+  }
+
+  // Create an Item (code, name, cost (0 if free), PLU? (true if PLU, false if
+  // UPC), date received, and date item expires)
+  // Add the item to the item_list Vector
+  // add the item code and quantity to the donated_sold HashMap
   public void add_item(String code, String name, double cost, boolean plu,
-    int days_until_exp, double qty);
-  public void show_items();
+    int days_until_exp, double qty){
+    Item _item = new Item();
+    _item.code = code;
+    _item.name = name;
+    _item.cost = cost;
+    _item.plu = plu;
+    _item.date_received = LocalDate.now();
+    _item.exp_date = _item.date_received.plus(days_until_exp, ChronoUnit.DAYS);
+    item_list.add(_item);
+    donated_sold.put(_item.code, qty);
+  }
+
+  // Print all of the items being donated/sold
+  public void show_items(){
+    NumberFormat formatter = NumberFormat.getCurrencyInstance();
+    String type;
+    double qty;
+    int count = 1;
+
+    for(Item it : item_list) {
+      type = it.plu ? "PLU":"UPC";
+      qty = donated_sold.get(it.code);
+      System.out.println(count + ".");
+      System.out.println("Item code: " + it.code);
+      System.out.println("Item name: " + it.name);
+      System.out.println("Item cost: " + formatter.format(it.cost));
+      System.out.println("Item PLU? " + type);
+      System.out.println("Item date received: " + it.date_received);
+      System.out.println("Item expiration date: " + it.exp_date);
+      if(it.plu){
+        System.out.println("Quantity: " + qty + " lbs");
+      }
+      else{
+        System.out.println("Quantity: " + qty + " units");
+      }
+      count++;
+    }
+  }
 }
