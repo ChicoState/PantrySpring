b0d3b9c5d675d5b4472f8b5cf6f2608c0fb7aed2
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-04-27 21:56:27 -0700

    updates code for working in IntelliJ

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,12 +1,9 @@
 package main.java.hello;
 
-//import java.time.LocalTime;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    //LocalTime currentTime = new LocalTime();
-    //System.out.println("The current local time is: " + currentTime);
-    hello.Greeter greeter = new hello.Greeter();
+    Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
   }
 }
