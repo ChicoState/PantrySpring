01e3929b4c41b7d38a14f53875001480a379ca2a
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-04-28 11:07:36 -0700

    updates client for hashmap with list of values

--- src/main/java/pantry/Client.java
+++ src/main/java/pantry/Client.java
@@ -5,10 +5,12 @@ public class Client {
     System.out.println("Spring Food Pantry");
     Provider provider = new Provider("Hunger Fighters", "organization");
     Provider provider2 = new Provider("Annie Bidwell", "community member");
+
     System.out.println("Provider: " + provider.get_provider_info());
     provider.add_item("94011", "Bananas", 0.99, true, 14, 10.5);
     provider.add_item("3424", "Carrots", 0.75, true, 21, 5.75);
     provider.show_items();
+
     System.out.println("Provider2: " + provider2.get_provider_info());
     provider2.add_item("894455000322", "Almond Butter", 0.0, false, 180, 5.0);
     provider2.show_items();
