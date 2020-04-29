7ec6e8d2479d867afcdf6909cd03800a912258c4
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-04-29 10:57:35 -0700

    made PURCHASE a final static value

--- src/main/java/pantry/Client.java
+++ src/main/java/pantry/Client.java
@@ -3,6 +3,7 @@ import java.time.LocalDate;
 
 public class Client {
   public static void main(String[] args) {
+    final String PURCHASE = "Purchase";
     System.out.println("Spring Food Pantry");
     Inventory inventory = Inventory.getInstance();
     inventory.displayInventory();
@@ -12,7 +13,7 @@ public class Client {
     provider.addItem("94011", "Bananas", 0.99, true, 14, 10.5);
     provider.addItem("3424", "Carrots", 0.75, true, 21, 5.75);
     provider.addItem("3424", "Carrots", 0.00, true, 21, 4.25);
-    Transaction trans = new Transaction(provider.getDonatedSold(), "Purchase", provider);
+    Transaction trans = new Transaction(provider.getDonatedSold(), PURCHASE, provider);
     trans.displayTransaction();
     inventory.displayInventory();
 
@@ -22,14 +23,14 @@ public class Client {
     Item apple2 = new Item().setCode("110022").setCost(5).setDateReceived(LocalDate.of(2020,04,06)).setExpiryDate(LocalDate.of(2020,05,10)).setName("Apple").setQty(12);
     provider1.addItem(apple1.getCode(), apple1.getName(), apple1.getCost(), apple1.isPLU(), apple1.daysUntilExp(), apple1.getQty());
     provider1.addItem(apple2.getCode(), apple2.getName(), apple2.getCost(), apple2.isPLU(), apple2.daysUntilExp(), apple2.getQty());
-    Transaction trans1 = new Transaction(provider1.getDonatedSold(), "Purchase", provider1);
+    Transaction trans1 = new Transaction(provider1.getDonatedSold(), PURCHASE, provider1);
     trans1.displayTransaction();
     inventory.displayInventory();
 
     Provider provider2 = new Provider("Annie Bidwell", "community member");
     System.out.println("Provider2: " + provider2.getProviderInfo());
     provider2.addItem("894455000322", "Almond Butter", 0.0, false, 180, 5.0);
-    Transaction trans2 = new Transaction(provider2.getDonatedSold(), "Purchase", provider2);
+    Transaction trans2 = new Transaction(provider2.getDonatedSold(), PURCHASE, provider2);
     trans2.displayTransaction();
     inventory.displayInventory();
   }
