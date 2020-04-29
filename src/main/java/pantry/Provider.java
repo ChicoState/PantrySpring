26c1f2b41efc06b2a2a5b7edc4cbf53556b0351e
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-04-29 10:39:10 -0700

    Updates Provider based on SonarCloud recommendations

--- src/main/java/pantry/Provider.java
+++ src/main/java/pantry/Provider.java
@@ -8,31 +8,31 @@ import java.util.HashMap;
 import java.util.ArrayList;
 
 public class Provider {
-  private final UUID _uuid;
-  private final String _name;
-  private final ProviderType _type;
-  private final HashMap<String, ArrayList<Item>> donated_sold = new HashMap<>();
+  private final UUID uuid;
+  private final String name;
+  private final ProviderType type;
+  private final HashMap<String, ArrayList<Item>> donatedSold = new HashMap<>();
 
   // Constructor
   // create a provider with random UUID, given name, and given type
   // (organization or community member)
   Provider(String name, String type) {
-    _uuid = UUID.randomUUID();
-    _name = name;
+    uuid = UUID.randomUUID();
+    this.name = name;
     if(type.equals("organization")){
-      _type = ProviderType.ORGANIZATION;
+      this.type = ProviderType.ORGANIZATION;
     }
     else{
-      _type = ProviderType.COMMUNITYMEMBER;
+      this.type = ProviderType.COMMUNITYMEMBER;
     }
   }
 
   // Return the UUID, name, and type of this Provider
-  public ArrayList<String> get_provider_info(){
+  public ArrayList<String> getProviderInfo(){
     ArrayList<String> provider = new ArrayList<>();
-    provider.add(_uuid.toString());
-    provider.add(_name);
-    provider.add(_type.name());
+    provider.add(this.uuid.toString());
+    provider.add(this.name);
+    provider.add(this.type.name());
     return provider;
   }
 
@@ -41,31 +41,31 @@ public class Provider {
   // provided at this time, and a generated id)
   // If the code doesn't already exist, add it as a key to donated_sold
   // Then add the item to the list of items with that code
-  public void add_item(String code, String name, double cost, boolean plu,
+  public void addItem(String code, String name, double cost, boolean plu,
     int days_until_exp, double qty){
-    Item _item = new Item();
-    _item.code = code;
-    _item.name = name;
-    _item.cost = cost;
-    _item.plu = plu;
-    _item.date_received = LocalDate.now();
-    _item.exp_date = _item.date_received.plus(days_until_exp, ChronoUnit.DAYS);
-    _item.qty = qty;
-    _item.id = UUID.randomUUID();
+    Item item = new Item();
+    item.code = code;
+    item.name = name;
+    item.cost = cost;
+    item.plu = plu;
+    item.dateReceived = LocalDate.now();
+    item.expDate = item.dateReceived.plus(days_until_exp, ChronoUnit.DAYS);
+    item.qty = qty;
+    item.id = UUID.randomUUID();
 
     //see if we already have an item list for current code (key)
-    ArrayList<Item> item_list = donated_sold.computeIfAbsent(_item.code, k -> new ArrayList<>());
     //if not create one and put it in the map
-    item_list.add(_item);
+    ArrayList<Item> itemList = donatedSold.computeIfAbsent(item.code, k -> new ArrayList<>());
+    itemList.add(item);
   }
 
-  public void show_items() {
+  public void showItems() {
     NumberFormat formatter = NumberFormat.getCurrencyInstance();
     String type;
 
-    for(HashMap.Entry<String, ArrayList<Item>> entry:donated_sold.entrySet()) {
-      int count = 1;
+    for(HashMap.Entry<String, ArrayList<Item>> entry:donatedSold.entrySet()) {
       System.out.println("Item Key " + entry.getKey() + ":");
+      int count = 1;
       for(Item it : entry.getValue()) {
         type = it.plu ? "PLU":"UPC";
         System.out.println("\t" + count + ".");
@@ -73,8 +73,8 @@ public class Provider {
         System.out.println("\tItem name: " + it.name);
         System.out.println("\tItem cost: " + formatter.format(it.cost));
         System.out.println("\tItem PLU? " + type);
-        System.out.println("\tItem date received: " + it.date_received);
-        System.out.println("\tItem expiration date: " + it.exp_date);
+        System.out.println("\tItem date received: " + it.dateReceived);
+        System.out.println("\tItem expiration date: " + it.expDate);
         if(it.plu){
           System.out.println("\tQuantity: " + it.qty + " lbs");
         }
@@ -86,7 +86,7 @@ public class Provider {
     }
   }
 
-  public HashMap<String, ArrayList<Item>> get_donated_sold(){
-    return donated_sold;
+  public HashMap<String, ArrayList<Item>> getDonatedSold(){
+    return donatedSold;
   }
 }
