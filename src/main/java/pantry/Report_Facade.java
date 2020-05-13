35e9bf0af2e7e2fec1dd359cc931979472aa3dcd
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-05-13 15:54:39 -0700

    adds Item Transaction History to Report

--- src/main/java/pantry/Report_Facade.java
+++ src/main/java/pantry/Report_Facade.java
@@ -7,12 +7,10 @@ public class Report_Facade {
 	public void getReport()
 	{
 		Expiration exp=new Expiration();
-		
 
 		System.out.println("**********************************************| Expiration Report |**********************************************");
 		System.out.println("Expired Items");
-		
-		
+
 		ArrayList<Item> expiredItems=exp.getExpiredItems();
 		System.out.println(String.format("%20s %25s %20s %25s %10s", "Item Code", "|", "Item Name", "|", "Expiry Date"));
 		System.out.println(String.format("%s","----------------------------------------------------------------------------------------------------------------"));		
@@ -22,8 +20,7 @@ public class Report_Facade {
 			System.out.printf("%20s %25s %20s %25s %10s%n", itm.getCode(), "|", itm.getName(), "|",itm.getExpDate());
 
 		}
-		
-		
+
 		System.out.println();
 		System.out.println("Items Expiring in 2 days -");
 		System.out.println(String.format("%20s %25s %20s %25s %10s", "Item Code", "|", "Item Name", "|", "Expiry Date"));
@@ -34,6 +31,17 @@ public class Report_Facade {
 			System.out.printf("%20s %25s %20s %25s %10s%n", itm.getCode(), "|", itm.getName(), "|",itm.getExpDate());
 
 		}
-		
+
+		TransactionHistory th = TransactionHistory.getInstance();
+		System.out.println("**********************************************| Measuring Impact |**********************************************");
+		System.out.println("\nTotal Amount of Food Donated:");
+		System.out.println("Number of packaged items: " + th.getPurchaseCount());
+		System.out.println("Total weight of weighted goods: " + th.getPurchaseWeight());
+		System.out.println("\nTotal Amount of Food Distributed to Students: ");
+		System.out.println("Number of packaged items: " + th.getCheckoutCount());
+		System.out.println("Total weight of weighted goods: " + th.getCheckoutWeight());
+
+		System.out.println("**********************************************| End Report |**********************************************");
+
 	}
 }
